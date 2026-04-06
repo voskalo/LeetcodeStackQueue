093a8630164c96a325cc052543fbe5439ec2bae1
Liybomyr Voskalo <voskalo.pn@ucu.edu.ua>
2026-04-06 12:36:53 +0300

    3rd task preparation

---
+++ Maximum Frequency Stack/main3.py
@@ -0,0 +1,19 @@
+'''Maximu Frequency Stack'''
+
+class FreqStack:
+    '''FreqStack class'''
+
+    def __init__(self):
+        ...
+
+    def push(self, val: int) -> None:
+        ...
+
+    def pop(self) -> int:
+        ...
+
+
+# Your FreqStack object will be instantiated and called as such:
+# obj = FreqStack()
+# obj.push(val)
+# param_2 = obj.pop()
