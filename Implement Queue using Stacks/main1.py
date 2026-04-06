1820fcb4ccfa72beb57459d315abf8247ac193f8
Liybomyr Voskalo <voskalo.pn@ucu.edu.ua>
2026-04-06 11:50:43 +0300

    1st task preparation

--- Implement Queue using Stacks/main1.py
+++ Implement Queue using Stacks/main1.py
@@ -1,1 +1,27 @@
 '''Implement Queue using Stacks'''
+
+class MyQueue:
+    '''class docstr'''
+
+    def __init__(self):
+        ...
+
+    def push(self, x: int) -> None:
+        ...
+
+    def pop(self) -> int:
+        ...
+
+    def peek(self) -> int:
+        ...
+
+    def empty(self) -> bool:
+        ...
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
