81550253f878d505bb97fd8f4aae7df00bf1da74
Liybomyr Voskalo <voskalo.pn@ucu.edu.ua>
2026-04-06 12:27:29 +0300

    wrote queue class

--- Implement Stack using Queues/main2.py
+++ Implement Stack using Queues/main2.py
@@ -11,6 +11,67 @@ class Node:
         self.next = None
 
 
+class Queue:
+    '''queue class'''
+
+    def __init__(self):
+        '''init'''
+
+        self.head = None
+        self.tail = None
+        self._size = 0
+
+    def is_empty(self):
+        '''validation if is empty'''
+        return self._size == 0
+
+
+    def push(self, x: int):
+        '''push funct'''
+
+        new = Node(x)
+
+        if self.is_empty():
+            self.head = self.tail = new
+
+        else:
+            self.tail.next = new
+            self.tail = new
+
+        self._size += 1
+
+
+    def pop(self):
+        '''pop funct'''
+
+        if self.is_empty():
+            return None
+
+        val = self.head.value
+        self.head = self.head.next
+
+        if self.head is None:
+            self.tail = None
+
+        self._size -= 1
+
+        return val
+
+
+    def peek(self):
+        '''peek funct'''
+
+        if self.head:
+            return self.head.value
+
+        return None
+
+
+
+    def size(self):
+        '''size funct'''
+        return self._size
+
 class MyStack:
 
     def __init__(self):
