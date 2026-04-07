477080963366695e97d74a760bf52a15d4364a0c
Liybomyr Voskalo <voskalo.pn@ucu.edu.ua>
2026-04-07 13:43:45 +0300

    wrote the FreqStack, passed_tests added, wrote all the methods

--- Maximum Frequency Stack/main3.py
+++ Maximum Frequency Stack/main3.py
@@ -51,13 +51,35 @@ class FreqStack:
     '''FreqStack class'''
 
     def __init__(self):
-        ...
+        '''init'''
+
+        self.freq = {}
+        self.group = {}
+        self.max_freq = 0
 
     def push(self, val: int) -> None:
-        ...
+        '''oush funct'''
+
+        freq = self.freq.get(val, 0) + 1
+        self.freq[val] = freq
+
+        if freq > self.max_freq:
+            self.max_freq = freq
+
+        if freq not in self.group:
+            self.group[freq] = Stack()
+
+        self.group[freq].push(val)
 
     def pop(self) -> int:
-        ...
+        '''pop funct'''
+        val = self.group[self.max_freq].pop()
+        self.freq[val] -= 1
+
+        if self.group[self.max_freq].is_empty():
+            self.max_freq -= 1
+
+        return val
 
 
 # Your FreqStack object will be instantiated and called as such:
